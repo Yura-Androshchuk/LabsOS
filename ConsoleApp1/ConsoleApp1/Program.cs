078797ad8a18184727b0afbaee20992106c05421
Yura-Androshchuk <yurathebestone@gmail.com>
2020-05-24 16:12:00 +0300

    Lab 5 added

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int[,,] b = new int[10, 10, 10];
+            //int res = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    for (int k = 0; k < 10; k++)
+                    {
+                        b[k, j, i]++;
+                    }
+                }
+            }
+            stopwatch.Stop();
+            TimeSpan timeSpan = TimeSpan.FromTicks(stopwatch.ElapsedTicks);
+            string timeInterval = timeSpan.ToString();
+            Console.WriteLine("Time spent: " + timeInterval);
+            Console.WriteLine();
+        }
+    }
+}
